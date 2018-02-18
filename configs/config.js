b7b2e7ee9b4c9b74c4b9c96015c8a3121da42d41
Rahul-Barick <libra2rahul@gmail.com>
2018-02-18 16:37:20 +0530

    Config Update

--- configs/config.js
+++ configs/config.js
@@ -13,16 +13,6 @@ module.exports = {
         session_secret: "!!CROPPER$$!!",
         api_record_limit: 20,
         base_url: "http://localhost:3001/",
-        s3: {
-            accessKeyId: "ID",
-            secretAccessKey: "Key",
-            bucket: "Rev"
-        },
-        s3_inspiration: {
-            accessKeyId: "Id",
-            secretAccessKey: "Key",
-            bucket: "Insp"
-        },
         upload_path: __dirname + '/../uploads',
     }
 };
